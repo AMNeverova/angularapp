73d600bd85382cc9696d3b665b791e40598044d1
AMNeverova <anastacia1302@yandex.ru>
2019-12-06 15:10:56 +0300

    added e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,55 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe('search block', () => {
+  browser.get(browser.baseUrl);
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+  it('should contain inserted value', () => {
+    let input = element(by.name('Type'));
+    input.sendKeys('repositories');
+    expect(input.getAttribute('value')).toBe('repositories');
+  })
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('angularapp app is running!');
-  });
+})
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
-});
+describe('results block', () => {
+  browser.get(browser.baseUrl);
+  let listItem = element(by.className('repo-item'));
+
+  it('should contain results list', () => {
+    let inputType = element(by.name('Type'));
+    let inputLang = element(by.name('Language'));
+    let inputText = element(by.className('input-text'));
+    let button = element(by.className('button-search'));
+  
+    inputType.sendKeys('repositories');
+    inputLang.sendKeys('Javascript');
+    inputText.sendKeys('tetris')
+    button.click();
+    expect(browser.isElementPresent(listItem)).toBe(true);
+  })
+
+  it('should show results in grid view on filter click', () => {
+    let gridOptionFilterEl = element(by.css('.grid'));
+    let gridListEl = element(by.className('grid-results'));
+    gridOptionFilterEl.click();
+    expect(browser.isElementPresent(gridListEl)).toBeTruthy();
+  })
+})
+
+describe('search page', () => {
+  browser.get(browser.baseUrl);
+
+  beforeEach( () => {
+    let myListLink = element(by.className('mylist-link'));
+    myListLink.click()
+  })
+
+  it ('should navigate to MyList page', ()=> {
+    expect(browser.getCurrentUrl()).toContain('my-list');
+  })
+
+  it('should navigate to MyList page and contain myList title', () => {
+    let titleEl = element(by.className('my-list_title'));
+    expect(browser.isElementPresent(titleEl)).toBeTruthy();
+  })
+})
